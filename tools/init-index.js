70376e3d9685719e7bf2b39f33704f14d7461571
Ágúst Ingi Kjartansson <agust.ingi.kjartansson@gmail.com>
2016-04-05 09:12:02 +0000

    new mappings

--- tools/init-index.js
+++ tools/init-index.js
@@ -9,6 +9,7 @@ var mappings = require('../lib/mappings.js');
 var argv = require('yargs').argv;
 
 var index = argv.i || false;
+var force = argv.f || false;
 
 if (!index || (typeof index !== 'string')){
   return console.log('No index specified, use -i [indexname]')
@@ -23,19 +24,43 @@ var options = {
 };
 
 
+function indexExists(index, callback){
+  client.indices.get({ index : index}, function (err, res){
+    if (err) return callback(null, res);
 
-client.indices.create({
-  index: index
-}, function (error, response) {
-  console.log(error, response);
+    callback('Index does exists', res);
+  })
+}
 
-  if (error) return console.log('CANNOT CREATE INDEX', error);
+indexExists(index, function (err, res){
+  if (err){
+    console.log('Index exists', index);
 
+    if (!force) return console.log('Use -f to force (it will delete the index)');
 
-  client.indices.putMapping(options, function (err, res){
-    if (err) return console.log('CANNOT PUT MAPPINGS', err);
+    return client.indices.delete({index : index}, function (err, res){
+      if (err) return console.log('Error deleteing index', index, err);
+      initIndex(index);
+    });
 
-    console.log('Alright', res);
-  });
-  // ...
+  }
+
+  initIndex(index);
+
 });
+
+function initIndex(index){
+  client.indices.create({
+    index: index
+  }, function (error, response) {
+
+    if (error) return console.log('CANNOT CREATE INDEX', error);
+
+    client.indices.putMapping(options, function (err, res){
+      if (err) return console.log('CANNOT PUT MAPPINGS', err);
+
+      console.log('Success!', res);
+    });
+    // ...
+  });
+}
